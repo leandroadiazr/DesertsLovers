dfcbef2045fcf4bc1716cf7f1917a5736473e769
Leandro Diaz <leandrodiaz@tql.com>
2023-11-13 08:17:11 -0500

    Initial Commit

---
+++ DesertsLovers/DesertsLoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertsLoversApp.swift
+//  DesertsLovers
+//
+//  Created by Leandro Diaz on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DesertsLoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
